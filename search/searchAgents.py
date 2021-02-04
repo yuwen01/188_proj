423e6fe9d4519641f913d1718733f09f5fe874dc
Yuwen <yuwen01@berkeley.edu>
2021-02-03 23:23:30 -0800

    who cares

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -542,6 +542,7 @@ class ClosestDotSearchAgent(SearchAgent):
         Returns a path (a list of actions) to the closest dot, starting from
         gameState.
         """
+        from search import aStarSearch
         # Here are some useful elements of the startState
         startPosition = gameState.getPacmanPosition()
         food = gameState.getFood()
@@ -549,6 +550,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
+        return aStarSearch(problem, heuristic=manhattanHeuristic)
         util.raiseNotDefined()
 
 class AnyFoodSearchProblem(PositionSearchProblem):
